298b410b4c8db58ece7725210e3a13aa232cf250
smowden <smowden@users.noreply.github.com>
2024-07-30 11:41:37 +0300

    wip

--- tlconfig.lua
+++ tlconfig.lua
@@ -1,8 +1,8 @@
 return {
   source_dir = "src",
-  include_dir = { "src" },
+  include_dir = { "src", "typedefs" },
   include = {
-    "**/*.tl"
+    "**/*.tl",
   },
   build_dir = "build-lua",
   global_env_def = "ao"
